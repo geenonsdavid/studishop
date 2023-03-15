c00e71644b698c4c5f0cc71b6a9c8bafb35aefbd
Geenons david <geenonsdavid@gmail.com>
2023-03-15 11:05:34 +0100

    add stateTypes.js

---
+++ src/functions/stateTypes.js
@@ -0,0 +1,14 @@
+export class stateTypes {
+    static string(value) {
+        return typeof value === 'string';
+    }
+    static number(value) {
+        return typeof value === 'number';
+    }
+    static boolean(value) {
+        return typeof value === 'boolean';
+    }
+}
+    
+
+
